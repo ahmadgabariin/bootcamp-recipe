aed2c678c0a9ba3d5389effc8e4e71949204fd68
ahmadgabariin <ahmadhamze.kawasaki@gmail.com>
2022-05-12 14:00:20 +0300

    Port number Replaced from 8000 to 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require(`express`)
 const path = require(`path`)
 const api = require(`./server/routers/api`)
 const app = express()
-const port = 8000
+const port = 8080
 
 
 app.use(express.json())
